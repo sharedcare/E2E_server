e81df19aa05dc10c70c787853acd84e08129a8f4
sharedcare <sharedcare@hotmail.com>
2018-04-29 14:58:08 -0500

    Added methods

--- deploy.py
+++ deploy.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+    return 'Hello World'
+
+@app.route('/register', methods=['POST'])
+def register():
+    body = request.get_json()
+    number = body['Number']
+    id = body['ID']
+    key_bundle = body['KeyBundle']
+    return '''<p>The number is {}</p>
+    <p>The id is {}</p>
+    <p>The key bundle is {}</p>'''.format(number, id, key_bundle)
+
+@app.route('/requestKey')
+def request_key():
+    query_params = request.args
+    id = query_params.get('id')
+    return '''<h1>The id is: {}</h1>'''.format(id)
+
+@app.route('/send', methods=['POST'])
+def send():
+    return 'Send'
+
+@app.route('/receive')
+def receive():
+    return 'Receive'
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
