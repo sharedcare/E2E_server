fb5b69e32f40eaf5fd4b3c81f7bf8162bcc806d3
sharedcare <sharedcare@hotmail.com>
2018-05-05 22:52:47 -0500

    Update mongo configs

--- deploy.py
+++ deploy.py
@@ -2,13 +2,20 @@ from flask import Flask, request, json, jsonify, render_template
 from flask_pymongo import PyMongo
 import random
 import os
+import re
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
 if 'MONGODB_URI' in os.environ:
-    app.config['MONGO_URI'] = os.environ['MONGODB_URI']
-    app.config['MONGO_DBNAME'] = os.environ['MONGODB_URI'].split('/')[-1]
+    mongo_uri = os.environ['MONGODB_URI']
+    app.config['MONGO_URI'] = mongo_uri
+    mongo_uri = re.split('//|/|:|@', mongo_uri)
+    app.config['MONGO_DBNAME'] = mongo_uri[-1]
+    app.config['MONGO_HOST'] = mongo_uri[4]
+    app.config['MONGO_PORT'] = mongo_uri[-2]
+    app.config['MONGO_USERNAME'] = mongo_uri[2]
+    app.config['MONGO_PASSWORD'] = mongo_uri[3]
 
 
 @app.route('/')
