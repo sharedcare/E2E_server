89e25f188b4878eee71f9576189228c28812aa4c
sharedcare <sharedcare@hotmail.com>
2018-05-05 21:54:13 -0500

    Added register key and request methods

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, json, jsonify, render_template
 from flask_pymongo import PyMongo
+import random
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -14,31 +15,57 @@ def e2e_home_page():
 def insert():
     query_params = request.args
     id = query_params.get('id')
-    mongo.db.test.insert_one({"id": id})
+    mongo.db.test.insert({"id": id})
     return "Success"
 
 @app.route('/get')
 def get():
     query_params = request.args
     id = query_params.get('id')
-    return mongo.db.test.find_one({"id": id})
+    result = mongo.db.test.distinct('id')
+    return jsonify({'result': result})
+
+@app.route('/remove')
+def remove():
+    mongo.db.test.remove()
+    return 'Removed'
+
 
 @app.route('/register', methods=['POST'])
 def register():
-    body = request.get_json()
-    number = int(body['Number'])
-    id = int(body['ID'])
-    key_bundle = body['KeyBundle']
-    data = {}
-    return '''<p>The number is {}</p>
-    <p>The id is {}</p>
-    <p>The key bundle is {}</p>'''.format(number, id, key_bundle)
+    pre_key_bundle = mongo.db.preKeyBundle
+    body = request.data
+    id = random.randint(0, 999999)
+    ids = pre_key_bundle.distinct('id')
+    while id in ids:
+        id = random.randint(0, 999999)
+
+    data = {"id": id, "preKeyBundle": body}
+    pre_key_bundle.insert(data)
+    response = app.response_class(
+        status=200,
+        mimetype='application/json',
+        response=json.dumps({'id': id})
+    )
+    return response
 
 @app.route('/requestKey')
 def request_key():
     query_params = request.args
-    id = query_params.get('id')
-    return '''<h1>The id is: {}</h1>'''.format(id)
+    id = int(query_params.get('id'))
+    preKeyBundle = mongo.db.preKeyBundle.find_one({'id': id})
+    print(preKeyBundle)
+    if preKeyBundle:
+        res_text = json.dumps({'id': preKeyBundle['id'], 'preKeyBundle': preKeyBundle['preKeyBundle'].decode(), 'status': 'success'})
+    else:
+        res_text = json.dumps({'status': 'failed', 'msg': 'no such id'})
+
+    response = app.response_class(
+        status=200,
+        mimetype='application/json',
+        response=res_text
+    )
+    return response
 
 @app.route('/send', methods=['POST'])
 def send():
