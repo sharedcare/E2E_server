587be1aa888088b020904a1cb5391c11f211f4cc
sharedcare <sharedcare@hotmail.com>
2018-05-08 18:24:02 -0500

    Add connect method, rewrite register method

--- deploy.py
+++ deploy.py
@@ -5,6 +5,10 @@ import os
 import time
 import string
 
+CUSTOM_HEADER = {'Access-Control-Allow-Origin': '*'}
+REQUEST = 'request'
+SUCCESS_STAT = 'success'
+FAIL_STAT = 'failed'
 
 app = Flask(__name__)
 
@@ -20,19 +24,6 @@ def e2e_home_page():
     return render_template('index.html',
                            online_users=online_users)
 
-@app.route('/insert')
-def insert():
-    query_params = request.args
-    id = query_params.get('id')
-    mongo.db.test.insert({"id": id})
-    return "Success"
-
-@app.route('/get')
-def get():
-    query_params = request.args
-    id = query_params.get('id')
-    result = mongo.db.test.distinct('id')
-    return jsonify({'result': result})
 
 @app.route('/remove')
 def remove():
@@ -42,51 +33,101 @@ def remove():
 
 @app.route('/register', methods=['POST'])
 def register():
-    pre_key_bundle = mongo.db.preKeyBundle
+    per_key_bundle = mongo.db.perKeyBundle
+    id_token = mongo.db.idToken
     body = request.data
     id = random.randint(0, 999999)
-    ids = pre_key_bundle.distinct('id')
+    ids = per_key_bundle.distinct('id')
     while id in ids:
         id = random.randint(0, 999999)
 
-    access_token = random_generator(32)
+    access_token = random_generator(size=32)
 
-    data = {"id": id, "preKeyBundle": body, "expireTime": time.time() + 60*60*3, "accessToken": access_token}
-    pre_key_bundle.insert(data)
+    pre_key_data = {"id": id, "preKeyBundle": body}
+    id_token_data = {"id": id, "accessToken": access_token, "expireTime": time.time() + 60*60*3}
+    per_key_bundle.insert(pre_key_data)
+    id_token.insert(id_token_data)
     response = app.response_class(
         status=200,
         mimetype='application/json',
-        headers={
-            'Access-Control-Allow-Origin': '*'
-        },
-        response=json.dumps({'id': id, "accessToken": access_token})
+        headers=CUSTOM_HEADER,
+        response=json.dumps({'id': id, "accessToken": access_token, 'status': SUCCESS_STAT})
     )
     return response
 
-@app.route('/requestKey')
-def request_key():
-    query_params = request.args
-    id = int(query_params.get('id'))
-    preKeyBundle = mongo.db.preKeyBundle.find_one({'id': id})
-    print(preKeyBundle)
-    if preKeyBundle:
-        res_text = json.dumps({'id': preKeyBundle['id'], 'preKeyBundle': preKeyBundle['preKeyBundle'].decode(), 'status': 'success'})
+
+@app.route('/connect', methods=['GET', 'POST'])
+def connect():
+    id_token_table = mongo.db.idToken
+    per_key_table = mongo.db.perKeyBundle
+    msg_table = mongo.db.msg
+    if request.method == 'GET':
+        query_params = request.args
+        access_token = query_params.get('accessToken')
+        id_token = id_token_table.find_one({'accessToken': access_token})
+
+        if id_token:
+            id = id_token['id']
+            msg = msg_table.find_one({'receiverId': id, 'type': REQUEST})
+            if msg:
+                res_text = json.dumps({'senderId': msg['senderId'], 'status': SUCCESS_STAT})
+            else:
+                res_text = json.dumps({'status': SUCCESS_STAT, 'msg': 'no connect request'})
+        else:
+            res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
+
+        response = app.response_class(
+            status=200,
+            mimetype='application/json',
+            headers=CUSTOM_HEADER,
+            response=res_text
+        )
+
+    elif request.method == 'POST':
+        body = request.get_json()
+        sender_token = body['accessToken']
+        receiver_id = int(body['receiverId'])
+        sender_id_token = id_token_table.find_one({'accessToken': sender_token})
+        if sender_id_token:
+            sender_id = sender_id_token['id']
+            receiver_id_token = id_token_table.find_one({'id': receiver_id})
+            if receiver_id_token:
+                receiver_id = receiver_id_token['id']
+                per_key_bundle = per_key_table.find_one({'id': receiver_id})
+                if per_key_bundle:
+                    msg_data = {'senderId': sender_id, 'receiverId': receiver_id, 'type': REQUEST}
+                    msg_table.insert(msg_data)
+                    res_text = json.dumps({'status': SUCCESS_STAT})
+                else:
+                    res_text = json.dumps({'status': FAIL_STAT, 'msg': 'key used'})
+            else:
+                res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
+
+        else:
+            res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
+
+        response = app.response_class(
+            status=200,
+            mimetype='application/json',
+            headers=CUSTOM_HEADER,
+            response=res_text
+        )
+
     else:
-        res_text = json.dumps({'status': 'failed', 'msg': 'no such id'})
+        response = app.response_class(
+            status=405,
+            mimetype='application/json',
+            headers=CUSTOM_HEADER,
+            response='method is not allow.'
+        )
 
-    response = app.response_class(
-        status=200,
-        mimetype='application/json',
-        headers={
-            'Access-Control-Allow-Origin': '*'
-        },
-        response=res_text
-    )
     return response
+
 
 @app.route('/send', methods=['POST'])
 def send():
     return 'Send'
+
 
 @app.route('/receive')
 def receive():
@@ -98,8 +139,10 @@ def receive():
     )
     return response
 
+
 def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 if __name__ == '__main__':
     app.run()
