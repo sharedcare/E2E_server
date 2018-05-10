4f734e1d3f5910d6e03b79192cde23dbc31184f5
iLtc <i@iltc.io>
2018-05-10 19:32:14 +0000

    Rewrote Connect

--- deploy.py
+++ deploy.py
@@ -7,6 +7,8 @@ import string
 
 CUSTOM_HEADER = {'Access-Control-Allow-Origin': '*'}
 REQUEST = 'request'
+CONFIRM = 'confirm'
+DECLINE = 'decline'
 SUCCESS_STAT = 'success'
 FAIL_STAT = 'failed'
 
@@ -23,7 +25,7 @@ def e2e_home_page():
     return render_template('index.html')
 
 
-@app.route('/remove')
+@app.route('/remove', methods=['POST'])
 def remove():
     mongo.db.preKeyBundle.remove()
     mongo.db.idToken.remove()
@@ -61,50 +63,60 @@ def connect():
     id_token_table = mongo.db.idToken
     per_key_table = mongo.db.perKeyBundle
     msg_table = mongo.db.msg
+    
+    query_params = request.args
+    access_token = query_params.get('accessToken')
+    
+    id = get_id_from_token(access_token)
+    
+    if not id:
+        res_text = json.dumps({'status': FAIL_STAT, 'msg': 'access_token not found'})
+        response = app.response_class(
+            status=200,
+            mimetype='application/json',
+            headers=CUSTOM_HEADER,
+            response=res_text
+        )
+        return response
+    
     if request.method == 'GET':
-        query_params = request.args
-        access_token = query_params.get('accessToken')
-        id_token = id_token_table.find_one({'accessToken': access_token})
-
-        if id_token:
-            id = id_token['id']
-            msg = msg_table.find_one({'receiverId': id, 'type': REQUEST})
-            if msg:
-                res_text = json.dumps({'senderId': msg['senderId'], 'status': SUCCESS_STAT})
-            else:
-                res_text = json.dumps({'status': SUCCESS_STAT, 'msg': 'no connect request'})
+        msg = msg_table.find_one({'receiverId': id, 'type': REQUEST})
+        if msg:
+            res_text = json.dumps({'senderId': msg['senderId'], 'status': SUCCESS_STAT})
         else:
-            res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
-
+            res_text = json.dumps({'status': SUCCESS_STAT, 'msg': 'no connect request'})
+        
         response = app.response_class(
             status=200,
             mimetype='application/json',
             headers=CUSTOM_HEADER,
             response=res_text
         )
+        
+        return response
 
-    elif request.method == 'POST':
+    if request.method == 'POST':
         body = request.get_json()
-        sender_token = body['accessToken']
         receiver_id = int(body['receiverId'])
-        sender_id_token = id_token_table.find_one({'accessToken': sender_token})
-        if sender_id_token:
-            sender_id = sender_id_token['id']
-            receiver_id_token = id_token_table.find_one({'id': receiver_id})
-            if receiver_id_token:
-                receiver_id = receiver_id_token['id']
-                per_key_bundle = per_key_table.find_one({'id': receiver_id})
-                if per_key_bundle:
-                    msg_data = {'senderId': sender_id, 'receiverId': receiver_id, 'type': REQUEST}
-                    msg_table.insert(msg_data)
-                    res_text = json.dumps({'status': SUCCESS_STAT})
-                else:
-                    res_text = json.dumps({'status': FAIL_STAT, 'msg': 'key used'})
+        
+        sender_id = id
+        
+        receiver_id_token = id_token_table.find_one({'id': receiver_id})
+        if receiver_id_token:
+            receiver_id = receiver_id_token['id']
+            
+            per_key_bundle = per_key_table.find_one({'id': receiver_id})
+            if per_key_bundle:
+                msg_data = {'senderId': sender_id, 'receiverId': receiver_id, 'type': REQUEST}
+                msg_table.insert(msg_data)
+                
+                res_text = json.dumps({'status': SUCCESS_STAT})
+                
             else:
-                res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
-
+                res_text = json.dumps({'status': FAIL_STAT, 'msg': 'sender key has been used'})
+                
         else:
-            res_text = json.dumps({'status': FAIL_STAT, 'msg': 'not found'})
+            res_text = json.dumps({'status': FAIL_STAT, 'msg': 'receiver_id not found'})
 
         response = app.response_class(
             status=200,
@@ -112,16 +124,46 @@ def connect():
             headers=CUSTOM_HEADER,
             response=res_text
         )
+        
+        return response
 
-    else:
+
+@app.route('/confirm', methods=['GET', 'POST'])
+def confirm():
+    id_token_table = mongo.db.idToken
+    per_key_table = mongo.db.perKeyBundle
+    msg_table = mongo.db.msg
+    
+    query_params = request.args
+    access_token = query_params.get('accessToken')
+    
+    id = get_id_from_token(access_token)
+    
+    if not id:
+        res_text = json.dumps({'status': FAIL_STAT, 'msg': 'access_token not found'})
         response = app.response_class(
-            status=405,
+            status=200,
             mimetype='application/json',
             headers=CUSTOM_HEADER,
-            response='method is not allow.'
+            response=res_text
         )
-
-    return response
+        return response
+    
+    if request.method == 'GET':
+        msg = msg_table.find_one({'receiverId': id, 'type': REQUEST})
+        if msg:
+            res_text = json.dumps({'senderId': msg['senderId'], 'status': SUCCESS_STAT})
+        else:
+            res_text = json.dumps({'status': SUCCESS_STAT, 'msg': 'no connect request'})
+        
+        response = app.response_class(
+            status=200,
+            mimetype='application/json',
+            headers=CUSTOM_HEADER,
+            response=res_text
+        )
+        
+        return response
 
 
 @app.route('/send', methods=['POST'])
@@ -142,6 +184,17 @@ def receive():
 
 def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def get_id_from_token(accessToken):
+    id_token_table = mongo.db.idToken
+    
+    id_token = id_token_table.find_one({'accessToken': accessToken})
+    
+    if id_token:
+        return id_token['id']
+    else:
+        return False
 
 
 if __name__ == '__main__':
